8373f82677a4009cdd21d7494ba9036c68f1321e
Naotaka Nakanishi <mochitsuki@mattarishitemota.com>
2013-12-14 12:34:50 +0900

    create rails4 sample app

---
+++ rails/4.0.2/hello/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Hello::Application.config.secret_key_base = 'ba607161c3d045a50bd6c6179fc37b3191bdc608e3749b85fd0acc99f7ab5222dedbbe18b52b9a56d65ba1289d2fb3f3976e64a733c251f2851b023be08651f6'
